db0e27d72b4d673f402d3d24a7dfe6ac4727621e
Kuurus <gab.guimbaud@gmail.com>
2024-08-29 23:54:46 +0200

    Add DB & ORM

---
+++ apps/backend/src/data-source.ts
@@ -0,0 +1,36 @@
+// src/data-source.ts
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+import { Profile } from "./entities/Profile";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: false,
+  entities: [User, Profile],
+  migrations: [],
+  subscribers: [],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Data Source has been initialized!");
+  })
+  .catch((err) => {
+    console.error("Error during Data Source initialization:", err);
+  });
