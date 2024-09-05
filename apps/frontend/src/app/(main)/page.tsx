44a920cff27beb368de4829bec11397cce849fe6
Thibault <dev@thibault.sh>
2024-09-05 15:04:17 +0200

    x

--- apps/frontend/src/app/(main)/page.tsx
+++ apps/frontend/src/app/(main)/page.tsx
@@ -1,5 +1,12 @@
-import ClientHome from "./components/ClientHome";
+// import ClientHome from "./components/ClientHome";
+
+import Link from "next/link";
 
 export default function Home() {
-  return <ClientHome />;
+  return (
+    <>
+      <Link href="/login-male">Login Male</Link>
+      <Link href="/profile-male">Profile Male</Link>
+    </>
+  );
 }
